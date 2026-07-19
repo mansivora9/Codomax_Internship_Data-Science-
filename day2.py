66052c375892ff6316de2a8bf6c5fc6078b9ba3d
Mansi Vora <mansivora279@gmail.com>
2026-07-19 20:29:54 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,62 @@
+# variables
+name = "Mansi"
+age = 20
+cgpa = 7.5
+print("------------------------")
+print("VARIABLES")
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)    
+print("------------------------")
+
+# data types
+
+integer = 100
+decimal = 10.5
+string = "How are you?"
+bool = True
+print("------------------------")
+print("DATA TYPES")
+print(type(integer))
+print(type(decimal))
+print(type(string))
+print(type(bool))
+print("------------------------")
+
+# operators
+a = 15
+b = 25
+
+print("------------------------")
+print("OPERATORS")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("------------------------")
+
+# loops
+    #for loop
+print("------------------------")
+print("FOR LOOP")
+for i in range(5):
+    print(i)
+print("------------------------")
+
+    # while loop
+print("------------------------")
+print("WHILE LOOP")
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+print("------------------------")
+
+# function
+print("------------------------")
+print("FUNCTION")
+def square(x):
+    return x * x
+
+print("Square of 9:", square(9))
+print("------------------------")
